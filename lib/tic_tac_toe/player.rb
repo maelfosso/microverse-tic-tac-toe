2df82a6e64091219a6af7a250f08c3bbe228ac21
TERVER AOSU <terveraosu@gmail.com>
2019-04-12 13:29:04 +0100

    made changes to the Player initialized method

--- lib/tic_tac_toe/player.rb
+++ lib/tic_tac_toe/player.rb
@@ -1,9 +1,9 @@
 module TicTacToe
     class Player
         attr_reader :name, :color
-        def initialize(value)
-            @name = value.fetch(:name)
-            @color = value.fetch(:color)
+        def initialize(input)
+            @name = input[:name]
+            @color = input[:color]
         end
     end
     
