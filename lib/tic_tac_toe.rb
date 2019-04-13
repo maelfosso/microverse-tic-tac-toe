5b5fd18952aece5b8416bffd7d3062631cc7b5a7
TERVER AOSU <terveraosu@gmail.com>
2019-04-13 15:14:07 +0100

    updated the executable file and the gemspec file

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,3 +6,21 @@ require_relative "./tic_tac_toe/board.rb"
 require_relative "./tic_tac_toe/cell.rb"
 require_relative "./tic_tac_toe/game.rb"
 require_relative "./tic_tac_toe/player.rb"
+
+class Instruct
+    puts "Welcome to tic tac toe"
+    
+    puts "Player one enter your name: "
+    player_one = gets.chomp
+    player_one = {color: "X", name: player_one}
+    
+    puts "Player two enter your name: "
+    player_two = gets.chomp
+    player_two = {color: "O", name: player_two}
+    
+    player_one = TicTacToe::Player.new(player_one)
+    player_two = TicTacToe::Player.new(player_two)
+    players = [player_one, player_two]
+    
+    TicTacToe::Game.new(players).play
+end
